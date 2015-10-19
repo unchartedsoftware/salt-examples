773e0c92487126fd1e88307608516f719cee9403
kbirk <birk.kevin@gmail.com>
2015-10-19 18:13:12 -0400

    Adds simple server and webapp to serve and display client rendered heatmaps.

---
+++ bin-example/webapp/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('app'));
+app.use('/tiles', express.static('../output'));
+app.use('/meta', express.static('../output'));
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Salt Bin example listening at http://localhost:%s', port);
+});
