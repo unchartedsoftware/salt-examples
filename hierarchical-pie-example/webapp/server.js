7ab62ed64e93ea119566c94d0c441e86d85c26b1
Sean McIntyre <s.mcintyre@xverba.ca>
2015-12-14 11:54:11 -0500

    Functional server for hpie example.

--- hierarchical-pie-example/webapp/server.js
+++ hierarchical-pie-example/webapp/server.js
@@ -4,11 +4,27 @@ var app = express();
 
 var db = new sqlite3.Database('../output/fs_stats.sqlite', sqlite3.OPEN_READONLY);
 
+function getChildren(dirname, done) {
+  if (dirname === '/') {
+    dirname = '';
+  }
+  db.all("SELECT * FROM fs_stats WHERE path = '."+dirname+"'", function(err, rows) {
+    done(err, rows);
+  });
+}
+
 app.use(express.static('app'));
 
 app.get('/data*', function(req, res) {
-  console.log(req.path);
-  //TODO query sqlite database for all directories UNDER the supplied path. need to strip /data
+  var root = req.path.substring(5);
+  console.log('GET ' + root);
+  getChildren(root, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 var server = app.listen(3000, function () {
